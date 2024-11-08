1548ce9c5beea1be36be6361c86f23f49a95bcde
Mayokun-Sofowora <sofoworamayokun@gmail.com>
2024-11-08 13:27:24 +0100

    Set up project directory

---
+++ lib/src/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class KaviApp extends StatelessWidget {
+  const KaviApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+var colorPalette = [
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.indigo,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey
+];
