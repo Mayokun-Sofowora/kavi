bc602c3ae29d16c67738dbd88d07a6dc6067f172
Mayokun-Sofowora <sofoworamayokun@gmail.com>
2024-11-08 15:01:15 +0100

    Update dependency

--- android/app/src/main/kotlin/com/example/kavi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kavi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/proj/kavi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.proj.kavi
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Register plugins with the Flutter engine
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
